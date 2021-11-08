6564fbf6530b8b37d64768abf2de7ec8f41b7819
d4nielj <d4niel.djm@gmail.com>
2021-11-07 20:29:09 -0500

    Add meta tags

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -6,6 +6,16 @@ const Layout = ({ children }) => {
     <Box as='main' bg='black.900' color='white'>
       <Head>
         <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <meta name='description' content="D4niel's landing page" />
+        <meta name='author' content='Daniel Jaramillo' />
+        <meta name='author' content='d4nielj' />
+        <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
+        <meta name='twitter:card' content='summary' />
+        <meta name='twitter:site' content='@d4niel_jm' />
+        <meta name='twitter:creator' content='@d4niel_jm' />
+        <meta name='twitter:image' content='/card.png' />
+        <meta property='og:site_name' content="D4nielJ's l4nding" />
+        <meta property='og:type' content='website' />
         <title>D4nielJ&apos;s l4nding</title>
       </Head>
       <Container>{children}</Container>
