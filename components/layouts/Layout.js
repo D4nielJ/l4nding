9b6af717a7a1ac0ff7949a8c4d79500701ec6bf4
d4nielj <d4niel.djm@gmail.com>
2021-11-07 20:39:57 -0500

    Change twitter meta tag to large image

--- components/layouts/Layout.js
+++ components/layouts/Layout.js
@@ -10,7 +10,7 @@ const Layout = ({ children }) => {
         <meta name='author' content='Daniel Jaramillo' />
         <meta name='author' content='d4nielj' />
         <link rel='shortcut icon' href='/favicon.ico' type='image/x-icon' />
-        <meta name='twitter:card' content='summary' />
+        <meta name='twitter:card' content='summary_large_image' />
         <meta name='twitter:site' content='@d4niel_jm' />
         <meta name='twitter:creator' content='@d4niel_jm' />
         <meta name='twitter:image' content='/card.png' />
